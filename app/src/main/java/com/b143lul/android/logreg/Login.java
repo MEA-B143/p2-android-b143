f03231813b5b7f38e500e507b3cc3c1c596b3dc4
NlGHT <NlGHT@users.noreply.github.com>
2018-04-24 10:55:00 +0200

    Init push, getField() not working yet

---
+++ app/src/main/java/com/b143lul/android/logreg/Login.java
@@ -0,0 +1,119 @@
+package com.b143lul.android.logreg;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login extends AppCompatActivity {
+    public static final String LOGIN_URL="http://b143servertesting.gearhostpreview.com/LogReg2/login.php";
+    public static final String KEY_EMAIL="email";
+    public static final String KEY_PASSWORD="password";
+    public static final String LOGIN_SUCCESS="success";
+    public static final String SHARED_PREF_NAME="tech";
+    public static final String EMAIL_SHARED_PREF="email";
+    public static final String LOGGEDIN_SHARED_PREF="loggedin";
+    public static final String ID_SHARED_PREF = "userid";
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button BtnLogin;
+    private boolean loggedIn=false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        editTextEmail=(EditText)findViewById(R.id.editText_email);
+        editTextPassword=(EditText)findViewById(R.id.editText_password);
+        BtnLogin=(Button)findViewById(R.id.btn_login);
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = editTextEmail.getText().toString().trim();
+                final String password = editTextPassword.getText().toString().trim();
+                login(email, password);
+            }
+        });
+    }
+
+    private void login(final String email, final String password) {
+        if (email.isEmpty()) {
+
+        }
+        if (!email.contains("@")) {
+
+        }
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, LOGIN_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.trim().startsWith(LOGIN_SUCCESS)){
+
+                            SharedPreferences sharedPreferences = Login.this.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                            editor.putBoolean(LOGGEDIN_SHARED_PREF, true);
+                            editor.putString(EMAIL_SHARED_PREF, email);
+                            editor.putString(KEY_PASSWORD, password);
+                            String[] retvals = response.trim().split(",");
+                            int id = Integer.parseInt(retvals[1].trim());
+                            editor.putInt(ID_SHARED_PREF, id);
+
+                            editor.commit();
+
+                            Intent intent = new Intent(Login.this, MainActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(Login.this, response, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> prams = new HashMap<>();
+                prams.put(KEY_EMAIL, email);
+                prams.put(KEY_PASSWORD, password);
+
+                return prams;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,Context.MODE_PRIVATE);
+        loggedIn = sharedPreferences.getBoolean(LOGGEDIN_SHARED_PREF, false);
+        String email = sharedPreferences.getString(EMAIL_SHARED_PREF, null);
+        String password = sharedPreferences.getString(KEY_PASSWORD, null);
+        if(loggedIn){
+            login(email, password);
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/b143lul/android/logreg/MainActivity.java
@@ -0,0 +1,199 @@
+package com.b143lul.android.logreg;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.b143lul.android.logreg.Login.ID_SHARED_PREF;
+import static com.b143lul.android.logreg.Login.LOGGEDIN_SHARED_PREF;
+import static com.b143lul.android.logreg.Login.SHARED_PREF_NAME;
+
+public class MainActivity extends AppCompatActivity{
+    private TextView scoreText;
+    private Button plusButton;
+    private Button minusButton;
+    HashMap<String,String> hashMap = new HashMap<>();
+    HttpParse httpParse = new HttpParse();
+    private String updateURL = "http://b143servertesting.gearhostpreview.com/Update/UpdateStudent.php";
+    private String receiveURL = "http://b143servertesting.gearhostpreview.com/GetVals/GetField.php";
+    ProgressDialog progressDialog;
+    private int id;
+    private String JSONOutput = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scoreText = (TextView)findViewById(R.id.scoreContainer);
+        plusButton = (Button)findViewById(R.id.plusButton);
+        minusButton = (Button)findViewById(R.id.minusButton);
+        scoreText.setText(getStringValue("score"));
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                changeScore(10);
+            }
+        });
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeScore(-10);
+            }
+        });
+        SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        if(sharedPreferences.getBoolean(LOGGEDIN_SHARED_PREF, false)) {
+            id = sharedPreferences.getInt(ID_SHARED_PREF, -1);
+        } else {
+            AlertDialog.Builder alertbox=new AlertDialog.Builder(MainActivity.this);
+            alertbox.setTitle("How tf did u get here???");
+            alertbox.setCancelable(false);
+            finish();
+        }
+    }
+    @Override
+    public boolean onKeyDown(int keyCode,KeyEvent event){
+
+        if (keyCode==KeyEvent.KEYCODE_BACK){
+            AlertDialog.Builder alertbox=new AlertDialog.Builder(MainActivity.this);
+            alertbox.setTitle("Do you want to exit? lol wtf");
+            alertbox.setCancelable(false);
+            alertbox.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alertbox.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+            alertbox.show();
+        }
+        return super.onKeyDown(keyCode,event);
+    }
+
+
+    // For Option Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu manu){
+        getMenuInflater().inflate(R.menu.main, manu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+        if(id==R.id.id_profile){
+            // Write own logic
+
+            return true;
+        }
+        if (id==R.id.id_set){
+
+            //Write own logic
+            return true;
+        }
+        return  true;
+    }
+
+
+    private void changeScore(final int scoreChange) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, updateURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jObject = new JSONObject(response);
+                            String aJsonString = jObject.getString("newScore");
+                            scoreText.setText(aJsonString);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Log.e();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> prams = new HashMap<>();
+                prams.put("id", Integer.toString(id));
+                prams.put("score", Integer.toString(scoreChange));
+
+                return prams;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private String getStringValue(final String column) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, receiveURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jObject = new JSONObject(response);
+                            JSONOutput = jObject.getString(column);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Log.e();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> prams = new HashMap<>();
+                prams.put("id", Integer.toString(id));
+                prams.put("field", column);
+
+                return prams;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+        return JSONOutput;
+    }
+}
