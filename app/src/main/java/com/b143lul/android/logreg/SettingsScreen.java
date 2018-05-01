c1f5fe91929d2777864ebdc7cae290ccf5d8176e
Victor <vroua17@student.aau.dk>
2018-05-01 13:11:23 +0200

    Updated the Settings screen and designed its layout

--- app/src/main/java/com/b143lul/android/logreg/SettingsScreen.java
+++ app/src/main/java/com/b143lul/android/logreg/SettingsScreen.java
@@ -1,10 +1,14 @@
 package com.b143lul.android.logreg;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SettingsScreen extends AppCompatActivity {
-
+    public static final String SETTINGS="tech";
+    SharedPreferences sharedPreferences = SettingsScreen.this.getSharedPreferences(SETTINGS, Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPreferences.edit();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
