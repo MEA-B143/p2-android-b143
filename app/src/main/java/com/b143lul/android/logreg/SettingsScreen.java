2fb399332958a72df1df4e8a60f1da16dc4d024d
Victor <vroua17@student.aau.dk>
2018-05-04 10:16:48 +0200

    Updated the code within the settings class and the override function

--- app/src/main/java/com/b143lul/android/logreg/SettingsScreen.java
+++ app/src/main/java/com/b143lul/android/logreg/SettingsScreen.java
@@ -12,18 +12,20 @@ public class SettingsScreen extends AppCompatActivity {
     public static final String SETTINGS="tech";
     SharedPreferences sharedPreferences = SettingsScreen.this.getSharedPreferences(SETTINGS, Context.MODE_PRIVATE);
     SharedPreferences.Editor editor = sharedPreferences.edit();
-    private boolean settings=false;
-    private boolean switch_settings=false;
-    private boolean text_settings=false;
+    Switch switch1;
+    Switch switch2;
+    TextView settings_title;
+    TextView notifications;
+    TextView sounds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference_screen);
-Switch switch1 = (Switch) findViewById(R.id.switch1);
-Switch switch2 = (Switch) findViewById(R.id.switch2);
-TextView title = (TextView) findViewById(R.id.textView3);
-TextView notifications = (TextView) findViewById(R.id.textView4);
-TextView sounds = (TextView) findViewById(R.id.textView5);
+        switch1 = (Switch) findViewById(R.id.switch1);
+        switch2 = (Switch) findViewById(R.id.switch2);
+        settings_title = (TextView) findViewById(R.id.textView3);
+        notifications = (TextView) findViewById(R.id.textView4);
+        sounds = (TextView)  findViewById(R.id.textView5);
     }
 }
