e1db11403cfb3478983fc8e8fff0842aa06f8848
ElinaTrier <36537127+ElinaTrier@users.noreply.github.com>
2018-05-08 14:20:19 +0200

    Revert "Pedometer"
    This reverts commit 421e8e6f26dff1518f3e864ab4f8db5c19f46a94.

--- app/src/main/java/com/b143lul/android/logreg/Pedometer.java
+++ app/src/main/java/com/b143lul/android/logreg/Pedometer.java
@@ -1,23 +1,10 @@
 package com.b143lul.android.logreg;
 
+import android.content.Context;
 <<<<<<< HEAD
-import android.annotation.TargetApi;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.io.IOException;
 =======
-import android.content.Context;
 import android.content.Intent;
+>>>>>>> b8e8e68394cecbd6d6b165bc2b76576e02d93396
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -25,91 +12,70 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
->>>>>>> b8e8e68394cecbd6d6b165bc2b76576e02d93396
 
+<<<<<<< HEAD
+public class Pedometer extends AppCompatActivity implements SensorEventListener {
+    private TextView tvSteps;
+    private SensorManager mSensorManager;
+    private Sensor mStepCounterSensor;
+    private Sensor mStepDetectorSensor;
+
+=======
 public class Pedometer extends AppCompatActivity {
     private TextView tvSteps;
 
-<<<<<<< HEAD
-    TextView steps;
-
-
-    Button startgame;
-
-
-    String countedStep;
-    String DetectedStep;
-
-    static final String State_Count = "Counter";
-    static final String State_Detect = "Detector";
-
-    boolean isServiceStopped;
-
-    private Intent intent;
-    private static final String TAG = "SensorEvent";
-
-    @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pedometer);
-
-        intent = new Intent(this, PedometerService.class);
-        init();
-
-    }
-
-    public void init() {
-
-
-        startgame = (Button)findViewById(R.id.startgame);
-        startgame.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                // start Service.
-                startService(new Intent(getBaseContext(), PedometerService.class));
-                // register our BroadcastReceiver by passing in an IntentFilter. * identifying the message that is broadcasted by using static string "BROADCAST_ACTION".
-                registerReceiver(broadcastReceiver, new IntentFilter(PedometerService.BROADCAST_ACTION));
-                isServiceStopped = false;
-            }
-        });
-
-        steps = (TextView)findViewById(R.id.steps);
-
-    }
-    protected void onPause() {
-        super.onPause();
-    }
-
-    protected void onResume() {
-        super.onResume();
-
-    }
-
-    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // call updateUI passing in our intent which is holding the data to display.
-            updateViews(intent);
-        }
-    };
-    private void updateViews(Intent intent) {
-        // retrieve data out of the intent.
-        countedStep = intent.getStringExtra("Counted_Step");
-        DetectedStep = intent.getStringExtra("Detected_Step");
-        Log.d(TAG, String.valueOf(countedStep));
-        Log.d(TAG, String.valueOf(DetectedStep));
-
-        steps.setText('"' + String.valueOf(countedStep) + '"' + " Steps Counted");
-
-=======
-    @Override
+>>>>>>> b8e8e68394cecbd6d6b165bc2b76576e02d93396
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedometer);
         tvSteps = (TextView) findViewById(R.id.tvSteps);
 
+<<<<<<< HEAD
+        mSensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        mStepCounterSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mStepDetectorSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+    }
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        float[] values = event.values;
+        int value = -1;
+
+        if (values.length > 0) {
+            value = (int) values[0];
+        }
+
+        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            tvSteps.setText("Step Counter Detected : " + value);
+        } else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            // For test only. Only allowed value is 1.0 i.e. for step taken
+            tvSteps.setText("Step Detector Detected : " + value);
+        }
+    }
+    protected void onResume() {
+
+        super.onResume();
+
+        mSensorManager.registerListener(this, mStepCounterSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mStepDetectorSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this, mStepDetectorSensor);
+    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+=======
         MessageReceiver receiver = new MessageReceiver(new Message());
 
         Intent intent = new Intent(this, PedometerService.class);
@@ -124,5 +90,3 @@ public class Pedometer extends AppCompatActivity {
 >>>>>>> b8e8e68394cecbd6d6b165bc2b76576e02d93396
     }
 }
-
-
