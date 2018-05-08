42af206c2cd8893594ea45ba583c3cbebc1fa198
Victor <vroua17@student.aau.dk>
2018-05-08 10:12:06 +0200

    Finally made the settings screen work!

--- app/src/main/java/com/b143lul/android/logreg/SettingsScreen.java
+++ app/src/main/java/com/b143lul/android/logreg/SettingsScreen.java
@@ -1,31 +1,57 @@
 package com.b143lul.android.logreg;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import static com.b143lul.android.logreg.Login.SHARED_PREF_NAME;
+
 public class SettingsScreen extends AppCompatActivity {
-    public static final String SETTINGS="tech";
-    SharedPreferences sharedPreferences = SettingsScreen.this.getSharedPreferences(SETTINGS, Context.MODE_PRIVATE);
-    SharedPreferences.Editor editor = sharedPreferences.edit();
+
     Switch switch1;
     Switch switch2;
     TextView settings_title;
     TextView notifications;
     TextView sounds;
+    boolean switch_trigger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference_screen);
+
+        SharedPreferences sharedPreferences = SettingsScreen.this.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
         switch1 = (Switch) findViewById(R.id.switch1);
         switch2 = (Switch) findViewById(R.id.switch2);
         settings_title = (TextView) findViewById(R.id.textView3);
         notifications = (TextView) findViewById(R.id.textView4);
-        sounds = (TextView)  findViewById(R.id.textView5);
+        sounds = (TextView) findViewById(R.id.textView5);
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (switch1.isChecked()) {
+                    notifications.setText("Switch notifications on");
+                } else {
+                    notifications.setText("Switch notifcations off");
+                }
+            }
+        });
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (switch2.isChecked()) {
+                    sounds.setText("Switch sounds on");
+                } else {
+                    sounds.setText("Switch sounds off");
+                }
+            }
+        });
     }
-}
+    }
